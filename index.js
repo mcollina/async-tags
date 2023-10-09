4ec79dc314d281b413ff26fa38d744f06856bcd6
Matteo Collina <hello@matteocollina.com>
2023-10-09 23:17:07 +0200

    rewrite without streams
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- index.js
+++ index.js
@@ -1,49 +1,29 @@
 import * as tags from 'common-tags'
-import { Readable } from 'node:stream'
-import { once } from 'node:events'
 
-function render (strings, ...values) {
+async function * render (strings, ...values) {
   let endReached = false
-  const stream = new Readable({
-    objectMode: true,
-    read () {
-      stream.emit('readCalled')
-    }
-  })
 
-  ;(async function () {
-    try {
-      for (let i = 0; i < strings.length; i++) {
-        const string = strings[i]
-        let value = values[i]
-        if (typeof value?.then === 'function') {
-          value = await value
-        }
-        if (value && value[Symbol.asyncIterator]) {
-          const res = tags.html.apply(null, [[string, ''], ''])
-          if (res) {
-            stream.push(res)
-          }
-          for await (const chunk of value) {
-            if (!stream.push(chunk)) {
-              await once(stream, 'readCalled')
-            }
-          }
-        } else {
-          const res = tags.html.apply(null, [[string, ''], value])
-          if (res) {
-            stream.push(res)
-          }
-        }
+  for (let i = 0; i < strings.length; i++) {
+    const string = strings[i]
+    let value = values[i]
+    if (typeof value?.then === 'function') {
+      value = await value
+    }
+    if (value && value[Symbol.asyncIterator]) {
+      const res = tags.html.apply(null, [[string, ''], ''])
+      if (res) {
+        yield res
       }
-
-      stream.push(null)
-    } catch (err) {
-      stream.destroy(err)
+      for await (const chunk of value) {
+        yield chunk
+      }
+    } else {
+      const res = tags.html.apply(null, [[string, ''], value])
+      if (res) {
+        yield res
+      }
     }
-  })()
-
-  return stream
+  }
 }
 
 export { render }

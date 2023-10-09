91cb6a64311a834c76ba676d13ec727e3ace8642
Matteo Collina <hello@matteocollina.com>
2023-10-10 00:17:23 +0100

    implement asynciterator protocol
    Signed-off-by: Matteo Collina <hello@matteocollina.com>

--- index.js
+++ index.js
@@ -1,29 +1,77 @@
 import * as tags from 'common-tags'
 
-async function * render (strings, ...values) {
-  let endReached = false
+class Renderer {
+  #strings
+  #values
+  #position
+  #currentIterator
 
-  for (let i = 0; i < strings.length; i++) {
-    const string = strings[i]
-    let value = values[i]
+  constructor (strings, values) {
+    this.#strings = strings
+    this.#values = values
+    this.#position = 0
+    this.#currentIterator = null
+  }
+
+  async next () {
+    if (this.#currentIterator !== null) {
+      const { value, done } = await this.#currentIterator.next()
+      if (done) {
+        this.#currentIterator = null
+      }
+      if (value) {
+        return { value, done: false }
+      }
+      // TODO handle an edge case where value: undefined and done: false
+    }
+
+    if (this.#position === this.#strings.length) {
+      return { value: undefined, done: true }
+    }
+
+    const string = this.#strings[this.#position]
+    let value = this.#values[this.#position]
+    this.#position++
+
     if (typeof value?.then === 'function') {
       value = await value
     }
     if (value && value[Symbol.asyncIterator]) {
+      this.#currentIterator = value[Symbol.asyncIterator]()
+
       const res = tags.html.apply(null, [[string, ''], ''])
       if (res) {
-        yield res
+        return { value: res, done: false }
       }
-      for await (const chunk of value) {
-        yield chunk
-      }
+
+      return this.next()
     } else {
       const res = tags.html.apply(null, [[string, ''], value])
       if (res) {
-        yield res
+        return { value: res, done: false }
       }
+      return this.next()
     }
   }
+
+  [Symbol.asyncIterator] () {
+    return this
+  }
 }
+
+function render (strings, ...values) {
+  // We must ensure that none of the promises are fire and forget.
+  // We are forking the promise chain on purpose.
+  for (let i = 0; i < values.length; i++) {
+    const value = values[i]
+    if (typeof value?.catch === 'function') {
+      value.catch(nop)
+    }
+  }
+
+  return new Renderer(strings, values)
+}
+
+function nop () {}
 
 export { render }
--- test.js
+++ test.js
@@ -2,6 +2,7 @@ import { test } from 'node:test'
 import assert from 'node:assert'
 import { render } from './index.js'
 import * as tags from 'common-tags'
+import { setImmediate as immediate } from 'node:timers/promises'
 
 test('async rendering', async (t) => {
   const firstChunk = `
@@ -76,4 +77,83 @@ test('nested async rendering', async (t) => {
     assert.strictEqual(chunk, tags.html.apply(null, [[chunks.shift()]]))
   }
   assert.strictEqual(chunks.length, 0)
+})
+
+test('layouts', async () => {
+  function layout1 (next) {
+    return render`
+      <DOCTYPE html>
+      <html>
+        <head>
+          <title>test</title>
+        </head>
+        <body>
+        ${next}
+        </body>
+      </html>
+    `
+  }
+
+  // Async function on purpose
+  async function h1 (text) {
+    return render`<h1>${text}</h1>`
+  }
+
+  const stream = layout1(h1(Promise.resolve('hello')))
+
+  const firstChunk = `
+  <DOCTYPE html>
+  <html>
+    <head>
+      <title>test</title>
+    </head>
+    <body>
+`
+
+  const secondChunk = `
+    </body>
+  </html>
+  `
+
+  const chunks = [
+    firstChunk,
+    '<h1>hello',
+    '</h1>',
+    secondChunk
+  ]
+
+  for await (const chunk of stream) {
+    assert.strictEqual(chunk, tags.html.apply(null, [[chunks.shift()]]))
+  }
+  assert.strictEqual(chunks.length, 0)
+})
+
+test('errors in layouts', async () => {
+  function layout1 (next) {
+    return render`
+      <DOCTYPE html>
+      <html>
+        <head>
+          <title>test</title>
+        </head>
+        <body>
+        ${next}
+        </body>
+      </html>
+    `
+  }
+
+  // Async function on purpose
+  async function h1 (text) {
+    return render`<h1>${text}</h1>`
+  }
+
+  const stream = layout1(h1(Promise.reject(new Error('kaboom'))))
+
+  await immediate()
+
+  await assert.rejects((async () => {
+    for await (const chunk of stream) {
+    }
+  })())
 })
